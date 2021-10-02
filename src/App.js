27a513c1026aab19a019d876b20e67ef0a8c318d
email <phamson061297@gmail.com>
2021-10-02 19:11:34 -0300

    updating app with shoppage collection preview comp and collection item comp

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
-
+import ShopPage from "./pages/shop/shop.component";
+import "./App.css";
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
