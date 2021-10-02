a8168d78e89320f873f839cdcfaf6d5756a57aaf
email <phamson061297@gmail.com>
2021-10-02 19:36:12 -0300

    Adding header

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import "./App.css";
+import Header from "./components/header/header.component";
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
--- src/components/collection-preview.component/collection-preview.component.jsx
+++ src/components/collection-preview.component/collection-preview.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./collection-preview.style.scss";
 import CollectionItem from "./../collection-item/collection-item.component";
+
 const CollectionPreview = ({ title, items }) => {
   return (
     <div className="collection-preview">
