574e626c570257aee8a7a3027bca6e43081f3de8
email <phamson061297@gmail.com>
2022-01-09 21:59:09 -0400

    adding user.types to extract cases

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,8 @@
+import { UserActionTypes } from "./user.types";
+
 //take one parameter as the subject that we are selecting
 const setCurrentUser = (user) => {
-  return { type: "SET_CURRENT_USER", payload: user };
+  return { type: UserActionTypes.SET_CURRENT_USER, payload: user };
 };
 
 export default setCurrentUser;
