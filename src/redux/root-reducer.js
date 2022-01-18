c3de9b9e8d2122451df9ef39b12bb3c2ca09aeba
email <phamson061297@gmail.com>
2022-01-18 12:53:31 -0400

    Moving Directory React state into Redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // saying that we want the default storage to be local storage
 //JSON object represent any possible configuration that persist can use
@@ -15,6 +16,7 @@ const rootReducer = combineReducers({
   //Root Reducer is basically the STATE, or a piece of data that the store is made of, When mapStateToProp, state is root reducer
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
